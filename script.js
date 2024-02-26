2a4e8b9bcf987571c7be96cdb40c9bbe5c374672
patelsweta2 <80040744+patelsweta2@users.noreply.github.com>
2024-02-26 20:57:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 //your JS code here. If required.
+
+const squares = document.querrySelectorAll('.square');
+
+function hoverEffect(selectedSquare){
+	squares.forEach(square => {
+		if(square !== selectedSquare){
+			square.style.backgroundColor = 'brown';
+		}
+	});
+}
+
+squares.forEach(square => {
+	square.addEventListener('mouseover', function()){
+		hoverEffect(this);
+	}
+})
