340b8a6fcaa5ac2f61a5aadc3438c20f4020ab08
patelsweta2 <80040744+patelsweta2@users.noreply.github.com>
2024-02-26 21:01:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,17 +1,16 @@
-//your JS code here. If required.
+    const squares = document.querySelectorAll('.square');
 
-const squares = document.querrySelectorAll('.square');
+    function hoverEffect(selectedSquare) {
+      squares.forEach(square => {
+		  square.style.backgroundColor = '#E6E6FA';
+        if (square !== selectedSquare) {
+          square.style.backgroundColor = 'brown';
+        }
+      });
+    }
 
-function hoverEffect(selectedSquare){
-	squares.forEach(square => {
-		if(square !== selectedSquare){
-			square.style.backgroundColor = 'brown';
-		}
-	});
-}
-
-squares.forEach(square => {
-	square.addEventListener('mouseover', function()){
-		hoverEffect(this);
-	}
-})
+    squares.forEach(square => {
+      square.addEventListener('mouseover', function() {
+        hoverEffect(this);
+      });
+    });
