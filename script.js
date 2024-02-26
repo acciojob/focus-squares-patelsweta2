70e3c6ee4f50185c9a8832ad597f302c82091d9f
patelsweta2 <80040744+patelsweta2@users.noreply.github.com>
2024-02-26 21:07:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,15 +2,25 @@
 
     function hoverEffect(selectedSquare) {
       squares.forEach(square => {
-		  square.style.backgroundColor = '#E6E6FA';
+		  // square.style.backgroundColor = '#E6E6FA';
         if (square !== selectedSquare) {
-          square.style.backgroundColor = 'brown';
+          square.style.backgroundColor = '#6F4E37';
         }
+      });
+    }
+
+	function resetColors() {
+      squares.forEach(square => {
+        square.style.backgroundColor = '#E6E6FA';
       });
     }
 
     squares.forEach(square => {
       square.addEventListener('mouseover', function() {
         hoverEffect(this);
+      });
+
+		square.addEventListener('mouseout', function() {
+        resetColors();
       });
     });
